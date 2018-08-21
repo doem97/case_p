3165c9f616f73548f95f552e5d5b9776f9e4c843
doem97 <doem1997@gmail.com>
2018-08-22 01:18:05 +0800

    Translate all comments into english and save with UTF-8 encoding.

--- DLlist.cpp
+++ DLlist.cpp
@@ -1,6 +1,6 @@
 #include "allmyfile.h"
 
-/*初始化链表新节点的数据域和指针域*/
+/*initialize a dll-type node*/
 void init_dl_list_node(dl_list_node * new_node, int cached_key_1, int cached_value_1, int cached_pkt_1, dl_list_head * list_head_p_temp)
 {
     new_node->next         = NULL;
@@ -13,13 +13,12 @@ void init_dl_list_node(dl_list_node * new_node, int cached_key_1, int cached_val
 }
 
 
-/*插入到链表最前面*/
+/*insert a node to the head of the list*/
 void insert_node_head(dl_list_head * list_head, dl_list_node * new_node)
 {
-    //list_head表示要插入的链表的表头，insert_node指向要插入的节点
     if(list_head->tail == NULL)
     {
-        //链表为空，加入第一个节点
+        //if a list is empty, insert a new node
         list_head->head = list_head->tail = new_node;
     }
     else
@@ -34,7 +33,7 @@ void insert_node_head(dl_list_head * list_head, dl_list_node * new_node)
     return;
 }
 
-/*删除链表末尾的节点，只能在cache满时用,cache size >> 1*/
+/*delete the node in the tail*/
 int delete_node_tail(dl_list_head * list_head)
 {
     dl_list_node * temp_node;
@@ -49,22 +48,22 @@ int delete_node_tail(dl_list_head * list_head)
     return i;
 }
 
-/*给出指针，删除链表某节点*/
+/*delete a node in the list*/
 void delete_node_pointed(dl_list_head * list_head_p, dl_list_node * list_ptr)
 {
-    if(list_ptr->next == NULL && list_ptr->previous == NULL)//链表只有一个元素
+    if(list_ptr->next == NULL && list_ptr->previous == NULL)//if a list only have one element
     {
         delete list_ptr;
         list_head_p->head = NULL;
         list_head_p->tail = NULL;
     }
-    else if(list_ptr->next == NULL)//删除链表尾部元素
+    else if(list_ptr->next == NULL)//delete the tail of the list
     {
         list_ptr->previous->next = NULL;
         list_head_p->tail = list_ptr->previous;
         delete list_ptr;
     }
-    else if(list_ptr->previous == NULL)//删除链表头部元素
+    else if(list_ptr->previous == NULL)//delete the head of the list
     {
         list_ptr->next->previous = NULL;
         list_head_p->head = list_ptr->next;
@@ -82,9 +81,8 @@ void delete_node_pointed(dl_list_head * list_head_p, dl_list_node * list_ptr)
     return;
 }
 
-/*插入到链表某节点前面*/
+/*insert a new node before an old node*/
 void insert_data_before(dl_list_node * old_node, dl_list_node * new_node)
 {
-    //old_node指的已有的某节点，new_node指的被插入的节点
     return;
 }
--- DLlist.h
+++ DLlist.h
@@ -1,42 +1,42 @@
 #ifndef DLLIST_H_INCLUDED
 #define DLLIST_H_INCLUDED
 
-//双向链表节点结构
+//double linked list node structure
 struct dl_list_node
 {
-    //数据域
+    //data field
     int cached_key;    //Flow_ID
     int cached_value;  //counts
     int cached_pkt;
-    //指针域
+    //pointer field
     dl_list_node * previous;
     dl_list_node * next;
     struct dl_list_head * list_head_p;//point to the list head
 };
 
-//双向链表的表头结构
+//list head of double linked list
 struct dl_list_head
 {
-    //数据域
-    int list_length;   //链表长度
-    //指针域
+    //data field
+    int list_length;
+    //pointer field
     dl_list_node * head;
     dl_list_node * tail;
 };
 
-/*初始化链表新节点的数据域和指针域*/
+/*initialize a double linked list node*/
 void init_dl_list_node(dl_list_node * new_node, int cached_key_1, int cached_value_1, int cached_pkt_1, dl_list_head * list_head_p_temp);
 
-/*插入到链表最前面*/
+/*insert a node to the head of the dll(double linked list)*/
 void insert_node_head(dl_list_head * list_head, dl_list_node * new_node);
 
-/*删除链表末尾的节点*/
+/*delete a node which at the tail of a dll*/
 int delete_node_tail(dl_list_head * list_head);
 
-/*给出指针，删除链表某节点*/
+/*delete a node in a dll with this node's pointer given*/
 void delete_node_pointed(dl_list_head * list_head_p, dl_list_node * list_ptr);
 
-/*插入到链表某节点前面*/
+/*insert a node at the top of the dll*/
 void insert_data_before(dl_list_node * old_node, dl_list_node * new_node);
 
 
@@ -44,7 +44,3 @@ void insert_data_before(dl_list_node * old_node, dl_list_node * new_node);
 
 
 #endif // DLLIST_H_INCLUDED
-
-
-
-
